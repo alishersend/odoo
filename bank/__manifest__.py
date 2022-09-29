f45f9b9d2dc34f4020f156fff763e8d7508deb47
alishersend <112221226+alishersend@users.noreply.github.com>
2022-09-30 00:24:28 +0600

    Add files via upload

--- bank/__manifest__.py
+++ bank/__manifest__.py
@@ -1,9 +1,11 @@
 {
     'name': 'bank',
+    'category': 'bank/manager',
     'sequence': 1,
     'application': True,
     'license': 'LGPL-3',
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/bank_clients_views.xml',
         'views/bank_clients_deposits_views.xml',
